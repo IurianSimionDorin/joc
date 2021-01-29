f36080c0674bfe78fde963b4581735aeff333cdf
Iurian Simion Dorin <simi_di@yahoo.com>
2021-01-29 14:37:26 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Board from './components/board/Board';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board />
+      </div>
+      <div className='game-info'>
+        <div>
+
+        </div>
+        <ol>
+          
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+export default App;
